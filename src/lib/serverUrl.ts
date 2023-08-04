f9660a49c1986a6f4e92d3ba2c43bdc9db59e6f5
ArturAronov <unionco@icloud.com>
2023-08-05 05:07:43 +0800

    updated prod api url

--- src/lib/serverUrl.ts
+++ src/lib/serverUrl.ts
@@ -1,4 +1,4 @@
 export const serverUrl =
   import.meta.env.VITE_NODE_ENV === "dev"
     ? "http://localhost:8888/v1"
-    : "https://cardem-1c6916426a4a.herokuapp.com/v1";
+    : "https://api.savorim.com/v1";
