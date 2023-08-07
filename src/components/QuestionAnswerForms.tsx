39f5992fba2b985c813c9e68fac1d85849c234f7
ArturAronov <unionco@icloud.com>
2023-08-08 05:03:10 +0800

    bug fixes in QuestionAnswerForms

--- src/components/QuestionAnswerForms.tsx
+++ src/components/QuestionAnswerForms.tsx
@@ -1,26 +1,26 @@
-import { Show, createSignal } from "solid-js";
+import {
+  For,
+  Index,
+  Show,
+  createComputed,
+  createEffect,
+  createSignal,
+} from "solid-js";
 import { AnswersT } from "../states/useQuestionAnswers";
 import DeleteIcon from "./icons/DeleteIcon";
 
 type PropsT = {
   question: string;
   answers: Array<AnswersT | string>;
+  onUpdateQuestion: (val: string) => void;
+  onDeleteAnswer: (deletedIndex: number) => void;
   onAddAnswer: (answer: AnswersT | string) => void;
-  onDeleteAnswer: (answers: Array<AnswersT | string>) => void;
-  onUpdateAnswer: (answers: Array<AnswersT | string>) => void;
-
-  // error: string;
-  // answers: Array<string>;
-  // updateQuestion: (val: string) => void;
-  // updateAnswers: () => void;
+  onUpdateAnswer: (updatedAnswer: string, updatedIndex: number) => void;
 };
 
 const QuestionAnswerForms = (props: PropsT) => {
-  const [answer, setAnswer] = createSignal<string>("");
-  const [question, setQuestion] = createSignal<string>(props.question);
-  const [answers, setAnswers] = createSignal<Array<AnswersT | string>>(
-    props.answers
-  );
+  const [answerInput, setAnswerInput] = createSignal<string>("");
+
   const [confirmDelete, setConfirmDelete] = createSignal<boolean>(false);
   const [confirmDeleteByIndex, setConfirmDeleteByIndex] = createSignal<
     number | null
@@ -31,87 +31,87 @@ const QuestionAnswerForms = (props: PropsT) => {
       <span class="label-text">Question</span>
       <input
         type="text"
-        value={question()}
+        value={props.question}
         class="input input-bordered w-full border-neutral/50 !outline-none focus:border-neutral"
-        onChange={(e) => {
-          // !!error && setError("");
-          setQuestion(e.currentTarget.value);
-        }}
+        onInput={(e) => props.onUpdateQuestion(e.target.value)}
       />
       <div class="divider" />
-      {props.answers.map((answer, index) => {
-        let _answer;
-        const isAnswerString = typeof answer === "string";
-        if (isAnswerString) _answer = answer;
-        else _answer = answer.name;
+      <For each={props.answers} fallback={""}>
+        {(answer, index) => {
+          console.log(answer);
+          let _answer;
+          const isAnswerString = typeof answer === "string";
+          if (isAnswerString) _answer = answer;
+          else _answer = answer.name;
 
-        return (
-          <div class="flex mt-2 mb-1">
-            <input
-              type="text"
-              value={_answer}
-              class="input input-bordered w-full rounded-r-none border-neutral/50 !outline-none focus:border-neutral"
-              onChange={(e) => {
-                if (isAnswerString) {
-                  const updatedAnswer = props.answers.map(
-                    (changedAnswer, changedIndex) => {
-                      if (changedIndex === index) return e.target.value;
-                      else return changedAnswer;
-                    }
-                  );
-                  props.onUpdateAnswer(updatedAnswer);
-                } else {
+          return (
+            <div class="flex mt-2 mb-1">
+              <input
+                disabled={index() === confirmDeleteByIndex()}
+                type="text"
+                value={_answer}
+                class="input input-bordered w-full rounded-r-none border-neutral/50 !outline-none focus:border-neutral truncate"
+                onInput={(e) =>
+                  props.onUpdateAnswer(e.currentTarget.value, index())
                 }
-              }}
-              // onChange={(e) => {
-              //   const updatedInput = answers.map((input, inputIndex) => {
-              //     if (index === inputIndex)
-              //       return (input = e.currentTarget.value);
-              //     else return input;
-              //   });
-              //   !!error && setError("");
-              //   setInputAnswers(updatedInput);
-              // }}
-            />
-            <button
-              class="btn btn-error rounded-l-none w-20 no-animation -ml-[1px]"
-              onClick={() => {
-                if (!confirmDelete()) {
-                  setConfirmDelete(true);
-                  setConfirmDeleteByIndex(index);
-                } else {
-                  const updatedAnswers = props.answers.filter(
-                    (answer, i) => i !== index && answer
-                  );
-                  props.onDeleteAnswer(updatedAnswers);
-                  setConfirmDeleteByIndex(null);
-                  setConfirmDelete(false);
-                }
-              }}
-            >
+              />
               <Show
-                when={confirmDelete() && confirmDeleteByIndex() === index}
-                fallback={<DeleteIcon />}
+                when={confirmDelete() && confirmDeleteByIndex() === index()}
+                fallback={
+                  <button
+                    title="Delete Questions"
+                    class="btn btn-error btn-outline [&>svg]:fill-error [&>svg]:hover:fill-primary-content rounded-l-none w-20 no-animation -ml-[1px]"
+                    onClick={() => {
+                      setConfirmDelete(true);
+                      setConfirmDeleteByIndex(index);
+                    }}
+                  >
+                    <DeleteIcon />
+                  </button>
+                }
               >
-                Confirm
+                <button
+                  title="Cancel Delete Answer"
+                  class="btn no-animation rounded-none text-xs"
+                  onClick={() => {
+                    setConfirmDelete(false);
+                    setConfirmDeleteByIndex(null);
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  title="Confirm Delete Answer"
+                  class="btn btn-error w-20 no-animation rounded-l-none -ml-[1px] text-xs"
+                  onClick={() => {
+                    // const updatedAnswers = props.answers.filter(
+                    //   (answer, i) => i !== index && answer
+                    // );
+                    props.onDeleteAnswer(index());
+                    setConfirmDeleteByIndex(null);
+                    setConfirmDelete(false);
+                  }}
+                >
+                  Confirm
+                </button>
               </Show>
-            </button>
-          </div>
-        );
-      })}
+            </div>
+          );
+        }}
+      </For>
       <div class="flex mt-2">
         <input
           type="text"
-          value={answer()}
+          value={answerInput()}
           class="input input-bordered w-full border-neutral/50 !outline-none rounded-r-none focus:border-neutral"
-          onChange={(e) => setAnswer(e.currentTarget.value)}
+          onInput={(e) => setAnswerInput(e.currentTarget.value)}
         />
         <button
-          class="btn btn-outline btn-info rounded-l-none no-animation w-20 -ml-[1px]"
+          class="btn btn-outline btn-info rounded-l-none no-animation w-20 -ml-[1px] text-xs"
           onClick={() => {
-            if (answer().trim().length) {
-              props.onAddAnswer(answer());
-              setAnswer("");
+            if (answerInput().trim().length) {
+              props.onAddAnswer(answerInput());
+              setAnswerInput("");
             }
           }}
         >
@@ -123,76 +123,3 @@ const QuestionAnswerForms = (props: PropsT) => {
 };
 
 export default QuestionAnswerForms;
-
-/*
-    <form>
-      <div class="mx-2 my-3">
-        <span class="label-text">Question</span>
-        <input
-          type="text"
-          value={question}
-          class="input input-bordered w-full focus:border-neutral/50 !outline-none"
-          onChange={(e) => {
-            !!error && setError("");
-            updateQuestion(e.currentTarget.value);
-          }}
-        />
-        <span class="label-text mt-2">Answers</span>
-
-        {answers.map((element, index) => {
-          return (
-            <div class="flex my-1">
-              <input
-                type="text"
-                value={element}
-                class="input input-bordered w-full focus:border-neutral/50 !outline-none"
-                onChange={(e) => {
-                  const updatedInput = answers.map((input, inputIndex) => {
-                    if (index === inputIndex)
-                      return (input = e.currentTarget.value);
-                    else return input;
-                  });
-                  !!error && setError("");
-                  setInputAnswers(updatedInput);
-                }}
-              />
-            </div>
-          );
-        })}
-        <span class="label-text mt-2">New Answer</span>
-        <div class="flex">
-          <input
-            type="text"
-            value={inputAnswer()}
-            class="input input-bordered w-full rounded-r-none focus:border-neutral/50 !outline-none"
-            onChange={(e) => setInputAnswer(e.currentTarget.value)}
-          />
-          <button
-            class="btn btn-outline btn-info rounded-l-none w-20 no-animation"
-            onClick={(e) => handleAddAnswer(e)}
-          >
-            Add
-          </button>
-        </div>
-      </div>
-      <p class="text-xs text-error font-bold text-center">{error()}</p>
-      <div class="mt-3 flex">
-        <button
-          class="btn text-error grow btn-ghost rounded-r-none rounded-tl-none rounded-bl-[15px] no-animation"
-          onClick={() => handleCancel()}
-        >
-          Cancel
-        </button>
-        <button
-          disabled={!!error()}
-          type="submit"
-          class="btn grow btn-ghost rounded-l-none rounded-tr-none rounded-br-[15px] no-animation"
-          onClick={(e) => handleSave(e)}
-        >
-          <Show when={isSaveLoading()} fallback="Save">
-            <LoadingSpinner size="default" />
-          </Show>
-        </button>
-      </div>
-    </form>
-*/
